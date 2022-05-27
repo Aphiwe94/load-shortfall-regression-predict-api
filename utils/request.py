c0b1d06936b33dc6360d7e18559e4622d8c60a92
ZanderM <zandermare10@gmail.com>
2022-05-27 10:58:04 +0200

    Mehhhhh1

--- utils/request.py
+++ utils/request.py
@@ -41,6 +41,8 @@ print(feature_vector_json)
 
 # url = 'http://http://192.168.0.110:5000/api_v0.1'
 url = 'http://54.155.105.0:5000/api_v0.1'
+#url = 'http://127.0.0.1:5000/api_v0.1'
+
 
 # Perform the POST request.
 print(f"Sending POST request to web server API at: {url}")
